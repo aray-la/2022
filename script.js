19ff87138a660a942ec7798cb61ca5105775984f
aray-la <61807715+aray-la@users.noreply.github.com>
2022-05-27 03:19:07 -0400

    1

--- script.js
+++ script.js
@@ -22,6 +22,36 @@ $( ".drag" ).draggable({
   } );
 
 
+function randomLeet() {
+  //console.log("hi");
+  var text = document.getElementById("face").innerHTML;
+  //console.log(text);
+  var newText = "";
+  var bracketFlag = false;
+  for (let i=0; i<text.length; i++) {
+    var c = text.charAt(i);
+    if (c === '<') {
+      bracketFlag = true;
+    }
+    else if (c === '>') {
+      bracketFlag = false;
+      newText += c;
+      continue;
+    }
+
+    if (bracketFlag) {
+      newText += c;
+      continue;
+    }
+
+    console.log(c);
+    var rand = Math.random() * 200;
+    console.log(rand);
+    var newC = "<span style='--leet-axis: " + rand + "; font-variation-settings: \"rdct\" var(--leet-axis);'>" + c + "</span>";
+    newText+= newC;
+  }
+  document.getElementById("face").innerHTML = newText;
+}
 
 
 
